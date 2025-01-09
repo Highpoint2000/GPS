b1eed838f12849ac3718c03b5c1386ba5b85c6fe
Highpoint2000 <168109804+Highpoint2000@users.noreply.github.com>
2025-01-09 15:41:32 +0100

    Update GPS-Plugin.js

--- GPS-Plugin.js
+++ GPS-Plugin.js
@@ -1,7 +1,7 @@
 // Plugin configuration, this is used in the administration when plugins are loaded
 var pluginConfig = {
     name: 'GPS Plugin',
-    version: '1.0',
+    version: '1.0a',
     author: 'Highpoint',
     frontEndPath: 'GPS/gps.js'
 }
